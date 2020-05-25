8f8b3104ce1d6c696f20e57d757cae801e638bfc
garzelli95 <fede.garzelli@gmail.com>
2020-05-25 19:08:12 +0100

    Add centroids initialization from cache

--- kmeans/src/main/java/it/unipi/hadoop/KMeans.java
+++ kmeans/src/main/java/it/unipi/hadoop/KMeans.java
@@ -1,10 +1,19 @@
 package it.unipi.hadoop;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -16,14 +25,39 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class KMeans {
 
+    private static final String cacheName = "centroids.txt";
+
     public static class KMeansMapper extends Mapper<Object, Text, IntWritable, WritableWrapper> {
 
         private static final Random rng = new Random(31);
 
         private int d;
+        private int k;
+        private List<Point> centroids;
 
         public void setup(Context context) throws IOException, InterruptedException {
             this.d = context.getConfiguration().getInt("kmeans.d", 7);
+            this.k = context.getConfiguration().getInt("kmeans.k", 13);
+
+            centroids = new ArrayList<>(k);
+            
+            Path cache = new Path(cacheName);
+            FileSystem fs = cache.getFileSystem(context.getConfiguration());
+
+            try (FSDataInputStream stream = fs.open(cache);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+
+                // read all lines from cache, use as centroids
+                String line = null;
+                while ((line = reader.readLine()) != null)
+                    centroids.add(Point.parseString(line));
+
+            } catch (IOException | ParseException e) {
+                // TODO handle exception
+            }
+
+            // TODO check if centroids are not k
+            
         }
 
         public void map(final Object key, final Text value, final Context context)
@@ -33,9 +67,11 @@ public class KMeans {
                 Point point = Point.parseString(value.toString());
 
                 int randomInt = rng.nextInt(20);
-                
+
                 if (point.getDimension() == d)
                     context.write(new IntWritable(randomInt), new WritableWrapper(point));
+
+                // ignore, i.e. do not emit, points with dimension different from d
             } catch (ParseException e) {
                 // ignore malformed points
             }
@@ -75,13 +111,19 @@ public class KMeans {
     }
 
     public static void main(String[] args) throws Exception {
+        int d = 6;
+        int k = 3;
         Configuration conf = new Configuration();
-        conf.setInt("kmeans.d", 6);
+        conf.setInt("kmeans.d", d);
+        conf.setInt("kmeans.k", k);
 
         Job job = Job.getInstance(conf, "kmeans");
 
         Path inputFile = new Path("data.txt");
+        Path cacheFile = new Path("centroids.txt");
         Path outputDir = new Path("output");
+
+        FileSystem fs = inputFile.getFileSystem(conf);
 
         FileInputFormat.addInputPath(job, inputFile);
         FileOutputFormat.setOutputPath(job, outputDir);
@@ -96,7 +138,51 @@ public class KMeans {
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(WritableWrapper.class);
 
+        List<Point> centroids = getRandomCentroids(k, fs, inputFile);
+        System.out.println("List of random centroids:");
+        for (Point point : centroids)
+            System.out.println("\t" + point);
+        
+        updateCache(centroids, fs, cacheFile);
+        System.out.println("Random centroids written to cache");
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    private static List<Point> getRandomCentroids(int k, FileSystem fs, Path file) {
+
+        List<Point> centroids = new ArrayList<>(k);
+
+        // TODO improve random selection of centroids
+
+        try (FSDataInputStream stream = fs.open(file);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+
+            // read k lines, parse, use as centroids
+            for (int i = 0; i < k; i++)
+                centroids.add(Point.parseString(reader.readLine()));
+
+        } catch (IOException | ParseException e) {
+            // TODO handle exception
+        }
+
+        return centroids;
+    }
+
+    private static void updateCache(List<Point> centroids, FileSystem fs, Path cache) {
+
+        try (FSDataOutputStream stream = fs.create(cache, true);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream))) {
+
+            // write each centroid in a line of the cache file
+            for (Point centroid : centroids) {
+                writer.write(centroid.toString());
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            // TODO handle exception
+        }
     }
 
 }
