7ffd6ef74869271d3f35815b9adfc02a9825e5ec
justausertestofgitit1 <taonorn@yahoo.it>
2020-06-23 19:01:28 +0200

    Changed to correct output

--- kmeans/script.sh
+++ kmeans/script.sh
@@ -5,5 +5,4 @@ mvn clean -f "${KMEANS}/pom.xml"
 mvn package -f "${KMEANS}/pom.xml"
 hadoop fs -rm -r output
 hadoop jar ${KMEANS}/target/KMeans-1.0-SNAPSHOT.jar it.unipi.hadoop.KMeans
-hadoop fs -cat output/part-r-00000
-
+hadoop fs -cat centroids.txt
