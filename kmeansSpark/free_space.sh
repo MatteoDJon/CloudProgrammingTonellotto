0e95b2d72ea90f8b1e1f623aa1be02be1057b686
garzelli95 <fede.garzelli@gmail.com>
2020-07-07 21:17:29 +0100

    Improved free_space.sh

--- kmeansSpark/free_space.sh
+++ kmeansSpark/free_space.sh
@@ -7,8 +7,16 @@ cd /opt/yarn/local/usercache/hadoop/filecache && sudo rm -rf *
 
 # hdfs dfsadmin -safemode leave
 
-# docker rmi $(docker images -a |  awk '{print $3}')
-# docker rm $(docker ps -a |  awk '{print $1}')
+# docker rmi $(docker images -a | awk '{print $3}')
+# docker rm $(docker ps -a | awk '{print $1}')
+
+namenode=$(cat /etc/hostname | grep "hadoop-namenode")
+if [ "${namenode}" = "hadoop-namenode" ]; then
+    stop-yarn.sh
+    start-yarn.sh
+else
+    echo "Run me in the namenode to stop and restart yarn"
+fi
 
 df -h
 
